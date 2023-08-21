f02e7f39e3a205093ced7b6344c0898066a506cf
Oxana Ozymlevskaya <oxanaozymlevskaya@MacBook-Air-Oxana.local>
2023-08-21 20:24:21 +0300

    origin

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Container.css';
+
+const Container = () => {
+  return (
+    <main className="container">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel justo vel turpis semper
+        fringilla. Proin id erat dolor. Vestibulum convallis justo ut justo posuere, vel faucibus odio
+        cursus. Integer ac suscipit nisl. Vestibulum non leo ac sapien viverra feugiat. Fusce interdum,
+        lorem id malesuada fermentum, sapien sapien pellentesque nisi, non blandit ex mauris id leo.
+        Vivamus lacinia dui eu facilisis.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel justo vel turpis semper
+        fringilla. Proin id erat dolor. Vestibulum convallis justo ut justo posuere, vel faucibus odio
+        cursus. Integer ac suscipit nisl. Vestibulum non leo ac sapien viverra feugiat. Fusce interdum,
+        lorem id malesuada fermentum, sapien sapien pellentesque nisi, non blandit ex mauris id leo.
+        Vivamus lacinia dui eu facilisis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel justo vel turpis semper
+        fringilla. Proin id erat dolor. Vestibulum convallis justo ut justo posuere, vel faucibus odio
+        cursus. Integer ac suscipit nisl. Vestibulum non leo ac sapien viverra feugiat. Fusce interdum,
+        lorem id malesuada fermentum, sapien sapien pellentesque nisi, non blandit ex mauris id leo.
+        Vivamus lacinia dui eu facilisis.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel justo vel turpis semper
+        fringilla. Proin id erat dolor. Vestibulum convallis justo ut justo posuere, vel faucibus odio
+        cursus. Integer ac suscipit nisl. Vestibulum non leo ac sapien viverra feugiat. Fusce interdum,
+        lorem id malesuada fermentum, sapien sapien pellentesque nisi, non blandit ex mauris id leo.
+        Vivamus lacinia dui eu facilisis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel justo vel turpis semper
+        fringilla. Proin id erat dolor. Vestibulum convallis justo ut justo posuere, vel faucibus odio
+        cursus. Integer ac suscipit nisl. Vestibulum non leo ac sapien viverra feugiat. Fusce interdum,
+        lorem id malesuada fermentum, sapien sapien pellentesque nisi, non blandit ex mauris id leo.
+        Vivamus lacinia dui eu facilisis.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel justo vel turpis semper
+        fringilla. Proin id erat dolor. Vestibulum convallis justo ut justo posuere, vel faucibus odio
+        cursus. Integer ac suscipit nisl. Vestibulum non leo ac sapien viverra feugiat. Fusce interdum,
+        lorem id malesuada fermentum, sapien sapien pellentesque nisi, non blandit ex mauris id leo.
+        Vivamus lacinia dui eu facilisis.
+      </p>
+    </main>
+  );
+};
+
+export default Container;
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Navigation.css';
+
+const Navigation = () => {
+  return (
+    <nav className="navigation">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
